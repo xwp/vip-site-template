0624bd8746053178cb7a9bb90e276df1d9a516b7
Kaspars Dambis <hi@kaspars.net>
2019-04-26 13:20:35 +0300

    Add some magic

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,39 +1,72 @@
+const exec = require('child_process').execSync;
 const parseArgs = require('minimist')
 const path = require('path')
 const fs = require('fs-extra')
 const git = require('simple-git')
+const ignore = require('ignore')
 
-const rootDir = path.join(__dirname, '..')
-
+// Parse the command-line arguments.
 const argv = parseArgs(process.argv.slice(2))
 const srcBranch = argv._[1]
 const upstreamRepo = argv._[0]
 
+const rootDir = path.join(__dirname, '..')
+
 const config = {
   src: {
-    from: rootDir,
+    repo: rootDir,
+    releaseDir: path.join(rootDir, 'deploy/src/public'),
     branch: srcBranch,
     dir: path.join(rootDir, 'deploy/src'),
   },
   dist: {
     repo: upstreamRepo,
+    repoDir: path.join(rootDir, 'deploy/dist-src'),
     branch: `deploy/${srcBranch}-${Math.round(new Date().getTime()/1000)}`,
     dir: path.join(rootDir, 'deploy/dist'),
   },
-  build: "npm install && npm run release",
-  distignore: path.join(rootDir, 'public/.distignore'),
+  build: 'npm install && npm run release',
+  distignore: path.join(rootDir, 'public/.distignore')
 }
 
-async function getHeadRevision(repoPath) {
-  return await git(config.src.dir).revparse();
-}
+// Always start with fresh source and release directories.
+fs.emptyDirSync(config.src.dir)
+fs.emptyDirSync(config.dist.dir)
+fs.emptyDirSync(config.dist.repoDir)
 
 // Checkout a fresh copy of the source branch in a new directory.
-fs.emptyDirSync(config.src.dir)
-fs.copySync(path.join(config.src.from, '.git'), path.join(config.src.dir, '.git'))
-git(config.src.dir).checkout(config.src.branch).reset('hard')
+fs.copySync(
+  path.join(config.src.repo, '.git'),
+  path.join(config.src.dir, '.git')
+)
+
+// Checkout a fresh source repository.
+git(config.src.dir)
+  .reset('hard')
+  .checkout(config.src.branch)
 
 // Checkout a fresh release repository.
-fs.emptyDirSync(config.dist.dir)
-git().clone(config.dist.repo, config.dist.dir).checkoutBranch(config.dist.branch, config.src.branch)
+git(config.dist.repoDir)
+  .clone(config.dist.repo, config.dist.repoDir)
+  .reset('hard')
+  .checkoutBranch(config.dist.branch, config.src.branch)
 
+// Run the build.
+exec(config.build, {
+  cwd: config.src.dir
+})
+
+// Copy the build to the release directory.
+fs.copySync(
+  config.src.releaseDir,
+  config.dist.dir,
+  {
+    filter: ignore().add(fs.readFileSync(config.distignore).toString()).createFilter()
+  }
+)
+
+// Now make the release directory a release repository.
+fs.copySync(
+  path.join(config.dist.repoDir, '.git'),
+  path.join(config.dist.dir, '.git')
+)
