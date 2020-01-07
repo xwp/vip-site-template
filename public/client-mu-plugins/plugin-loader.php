6d5c212a2a71a82802a33e33ff38b08ca716b6ba
Kaspars Dambis <hi@kaspars.net>
2020-01-07 16:15:43 +0200

    Don’t use WP globals

--- public/client-mu-plugins/plugin-loader.php
+++ public/client-mu-plugins/plugin-loader.php
@@ -8,14 +8,14 @@
  *
  * @var array
  */
-$plugins = [
+$mu_plugin_files = [
 	// 'example-plugin/plugin.php',
 ];
 
-foreach ( $plugins as $plugin ) {
-	$plugin_file = sprintf( '%s/%s', __DIR__, $plugin );
+foreach ( $mu_plugin_files as $mu_plugin_file ) {
+	$mu_plugin_file_path = sprintf( '%s/%s', __DIR__, $mu_plugin_file );
 
-	if ( file_exists( $plugin_file ) ) {
-		require_once $plugin_file;
+	if ( file_exists( $mu_plugin_file_path ) ) {
+		require_once $mu_plugin_file_path;
 	}
 }
