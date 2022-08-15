d859e252162e5f107fcee1cd9d54cef87dbeb8f1
Slava Vasenin <time2type@gmail.com>
2022-08-15 19:22:20 +0300

    Add object cache to use mu-plugin drop-ins

---
+++ object-cache.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Include the object cache from the VIP mu-plugin bundle.
+ *
+ * Used on local only.
+ *
+ */
+
+if ( file_exists( __DIR__ . '/mu-plugins/drop-ins/object-cache.php' ) ) {
+	require_once __DIR__ . '/mu-plugins/drop-ins/object-cache.php';
+}
