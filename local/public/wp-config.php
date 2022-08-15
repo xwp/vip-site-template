f534807607a1dbab5ef7f310af7c5212f28a38f6
Kaspars Dambis <hi@kaspars.net>
2022-08-15 19:58:35 +0300

    Use the WP container directly with Apache behind the scenes

--- local/public/wp-config.php
+++ local/public/wp-config.php
@@ -14,11 +14,26 @@ define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
 define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+
+// Set the default theme for new sites.
+define( 'WP_DEFAULT_THEME', 'twentytwentytwo' );
+
+// Enable offline mode to ensure it doesn't connect to WP.com.
+define( 'JETPACK_DEV_DEBUG', true ); // phpcs:ignore WordPressVIPMinimum.Constants.RestrictedConstants.DefiningRestrictedConstant
 
 // Use Composer and Git to update plugins and themes.
 define( 'DISALLOW_FILE_MODS', true );
 define( 'DISALLOW_FILE_EDIT', true );
 define( 'AUTOMATIC_UPDATER_DISABLED', true );
+
+// Keep the wp-contents outside of WP core directory.
+define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
+
+// Ensure object-cache.php knows where to load the assets from.
+if ( ! defined( 'WPMU_PLUGIN_DIR' ) && defined( 'WP_CONTENT_DIR' ) ) {
+	define( 'WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins' );
+}
 
 // Define the local environment.
 define( 'VIP_GO_APP_ENVIRONMENT', 'local' );
@@ -32,7 +47,7 @@ if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && 'https' === $_SERVER['HTTP_X
 $memcached_servers = [
 	'default' => [
 		'memcached:11211',
-	]
+	],
 ];
 
 // Include VIP-specific config.
