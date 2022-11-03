972fd740b15bc255698ebee3b188caa38fb668eb
Kaspars Dambis <hi@kaspars.net>
2022-11-03 13:33:37 +0200

    Fix reference

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
 	// Use a callback to avoid passing the changed file paths to the script.
 	'composer.*': () => 'npm run lint-composer',
-	'*.php': () => 'npm run lint:php',
+	'*.php': () => 'npm run lint-php',
 }
