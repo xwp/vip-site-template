f8edd74dbf38cb651be09f3775e0c51ebb2585e1
Kaspars Dambis <hi@kaspars.net>
2022-11-03 13:33:52 +0200

    Update to PHP8

---
+++ local/public/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Load WP from the managed sub-directory.
+ *
+ * Prevents the WP docker container from installing
+ * WP core files in this directory.
+ *
+ * @see https://github.com/docker-library/wordpress/blob/e665dbf6044556ace612e09ce1e01d92bb8d6a34/latest/php8.0/fpm/docker-entrypoint.sh#L71-L96
+ *
+ */
+
+require_once __DIR__ . '/wp/index.php';
