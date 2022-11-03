c6ba8e02c270b825cd39c2e1580793b3f0a7f206
Kaspars Dambis <hi@kaspars.net>
2022-11-03 16:10:23 +0200

    Fix cache path

--- object-cache.php
+++ object-cache.php
@@ -6,6 +6,6 @@
  *
  */
 
-if ( file_exists( __DIR__ . '/mu-plugins/drop-ins/object-cache-stable.php' ) ) {
-	require_once __DIR__ . '/mu-plugins/drop-ins/object-cache-stable.php';
+if ( file_exists( __DIR__ . '/mu-plugins/drop-ins/object-cache/object-cache-stable.php' ) ) {
+	require_once __DIR__ . '/mu-plugins/drop-ins/object-cache/object-cache-stable.php';
 }
