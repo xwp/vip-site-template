19463d70b1485ac938f36d588dd98bf18a314320
Kaspars Dambis <hi@kaspars.net>
2022-12-08 12:36:39 +0200

    Add the defaults for VIP env

--- vip-config/vip-config.php
+++ vip-config/vip-config.php
@@ -12,3 +12,42 @@
 
 // Enable Composer autoloader.
 require dirname( __DIR__ ) . '/wp-content/plugins/vendor/autoload.php';
+
+/**
+ * Set a high default limit to avoid too many revisions from polluting the database.
+ *
+ * Posts with extremely high revisions can result in fatal errors or have performance issues.
+ *
+ * Feel free to adjust this depending on your use cases.
+ */
+if ( ! defined( 'WP_POST_REVISIONS' ) ) {
+	define( 'WP_POST_REVISIONS', 100 );
+}
+
+/**
+ * Disable New Relic Browser instrumentation.
+ *
+ * By default, the New Relic extension automatically enables Browser instrumentation.
+ *
+ * This injects some New Relic specific javascript onto all pages on the VIP Platform.
+ *
+ * This isn't always desireable (e.g. impacts performance) so let's turn it off.
+ *
+ * If you would like to enable Browser instrumentation, please remove the lines below.
+ *
+ * @see https://docs.newrelic.com/docs/agents/php-agent/features/browser-monitoring-php-agent/#disable
+ * @see https://docs.wpvip.com/technical-references/tools-for-site-management/new-relic/
+ */
+if ( function_exists( 'newrelic_disable_autorum' ) ) {
+	newrelic_disable_autorum();
+}
+
+/**
+ * Set WP_DEBUG to true for all local or non-production VIP environments to ensure
+ * _doing_it_wrong() notices display in Query Monitor. This also changes the error_reporting level to E_ALL.
+ *
+ * @see https://wordpress.org/support/article/debugging-in-wordpress/#wp_debug
+ */
+if ( defined( 'VIP_GO_APP_ENVIRONMENT' ) && 'production' !== VIP_GO_APP_ENVIRONMENT && ! defined( 'WP_DEBUG' ) ) {
+	define( 'WP_DEBUG', true );
+}
