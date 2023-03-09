a53161cd55903699276f4ff0ca59b65df1c473f3
Kaspars Dambis <hi@kaspars.net>
2023-03-09 12:58:29 +0200

    Add the multisite config by default

--- local/public/wp-config.php
+++ local/public/wp-config.php
@@ -7,13 +7,19 @@
  * @package XWP\VipSiteTemplate
  */
 
+// Configured in docker-compose.yml.
 define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'wordpress' );
 define( 'DB_PASSWORD', 'wordpress' );
 define( 'DB_HOST', 'db' );
-
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
+
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', true );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
 
 define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_DISPLAY', true );
