fd8cd35c41a0b16a85f3b036aba7bb44b3bfc452
Kaspars Dambis <kaspars.dambis@xwp.co>
2023-11-07 19:25:31 +0200

    Update the include location per VIP mu-plugin changes

--- object-cache.php
+++ object-cache.php
@@ -7,6 +7,6 @@
  * @package XWP\Vip_Site_Template
  */
 
-if ( file_exists( __DIR__ . '/mu-plugins/drop-ins/object-cache/object-cache-stable.php' ) ) {
-	require_once __DIR__ . '/mu-plugins/drop-ins/object-cache/object-cache-stable.php';
+if ( file_exists( __DIR__ . '/mu-plugins/drop-ins/object-cache.php' ) ) {
+	require_once __DIR__ . '/mu-plugins/drop-ins/object-cache.php';
 }
